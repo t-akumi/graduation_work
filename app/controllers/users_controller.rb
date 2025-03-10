19e27ea11ab1aa6ca0b67f558b0c8963345095ea
takumi <sakatagintoki78013@gmail.com>
2025-03-10 10:48:31 +0900

    ログイン機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            auto_login(@user)
             redirect_to root_path
         else
             flash.now[:alert] = "登録に失敗しました"
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "entrance#index"
   resources :users
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
