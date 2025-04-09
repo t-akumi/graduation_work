19b81612258d8680ff3ee1f7c1d2a83223d3d492
takumi <sakatagintoki78013@gmail.com>
2025-04-09 09:16:44 +0900

    gmailを使っての送信設定に変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,15 @@ Rails.application.configure do
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :sendmail
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => '587',
+    :domain => 'smtp.gmail.com',
+    :authentication => 'plain',
+    :user_name => 'sakatagintoki78013@gmail.com',
+    :password => 'dmhsnhfsokuwqwbw'
+  }
 
 end
