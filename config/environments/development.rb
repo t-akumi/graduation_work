7b56dd6acafc53d318328ab8afce5665953bd775
takumi <sakatagintoki78013@gmail.com>
2025-04-09 09:35:42 +0900

    迷惑メールに分類されていたが、以前の設定で送信できていることを確認

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,20 +71,8 @@ Rails.application.configure do
   # config.generators.apply_rubocop_autocorrect_after_generate!
  
   #letter_opener
-  #config.action_mailer.delivery_method = :letter_opener_web
-  #config.action_mailer.perform_deliveries = true
-  #config.action_mailer.default_url_options = { host: 'localhost:3000' }
-
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address => 'smtp.gmail.com',
-    :port => '587',
-    :domain => 'smtp.gmail.com',
-    :authentication => 'plain',
-    :user_name => 'sakatagintoki78013@gmail.com',
-    :password => 'dmhsnhfsokuwqwbw'
-  }
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
 end
