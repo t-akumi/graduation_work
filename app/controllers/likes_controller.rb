4ba7194f23a3c5d62fb9abde69645f6a28bc2ca6
takumi <sakatagintoki78013@gmail.com>
2025-05-21 23:40:35 +0900

    いいね機能の非同期処理 turbo_stream

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,20 @@ class LikesController < ApplicationController
     def create
         @article = Article.find(params[:id])
         current_user.like_arts << @article
-        redirect_to params[:redirect_to] || articles_path
+        respond_to do |format|
+            format.turbo_stream
+            format.html { redirect_to params[:redirect_to] || articles_path }
+        end
     end
 
     def destroy
         @article = Article.find(params[:id])
         current_user.like_arts.destroy(@article)
         redirect_to params[:redirect_to] || articles_path
+        respond_to do |format|
+            format.turbo_stream
+            format.html { redirect_to params[:redirect_to] || articles_path }
+        end
     end
 
 end
